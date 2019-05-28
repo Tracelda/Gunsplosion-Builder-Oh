c68b8b8da52e72d82b675174eeee7ec94df51ec1
Olly Mills <kinglslbob@gmail.com>
2019-05-28 12:56:37 +0100

    weapon data types setup

---
+++ Gunsplosion Builder-Oh/Assets/Scripts/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public Weapon.bulletType currentType;
+    public bool active;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void activate(Weapon.bulletType type)
+    {
+        
+    }
+}
--- Gunsplosion Builder-Oh/Assets/Scripts/Weapon.cs
+++ Gunsplosion Builder-Oh/Assets/Scripts/Weapon.cs
@@ -4,6 +4,32 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    public enum weaponType { Straight, Bounce, Homing};
+
+    [System.Serializable]
+    public struct weaponStats
+    {
+        public weaponType type;
+        public float fireRate;
+        public bool active;
+        public int currentSlot;
+
+        public Sprite weaponSprite;
+        public Vector3 shotStartPosition;
+    }
+
+    public weaponStats Straight, Bounce, Homing;
+
+    [System.Serializable]
+    public struct bulletType
+    {
+        public Weapon.weaponType type;
+        public float damage;
+        public Sprite bulletShot;
+        public float lifespan;
+    }
+    public bulletType StraightShot, BounceShot, HomingShot;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +40,10 @@ public class Weapon : MonoBehaviour
     void Update()
     {
         
+    }
+
+    protected virtual void fire()
+    {
+
     }
 }
