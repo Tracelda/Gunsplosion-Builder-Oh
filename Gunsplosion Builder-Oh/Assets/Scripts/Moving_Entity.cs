0d78940b4afe791625ad33767a8982c083c02c67
DavidRMincer <33055698+DavidRMincer@users.noreply.github.com>
2019-05-28 13:52:44 +0100

    Movement and Jumping STARTED

---
+++ Gunsplosion Builder-Oh/Assets/Scripts/Moving_Entity.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving_Entity : MonoBehaviour
+{
+    public float            moveSpeed,
+                            jumpForce,
+                            distFromGround;
+    public string           GroundLayer;
+
+    private Rigidbody2D     rb;
+    private BoxCollider2D   playerCollider;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        playerCollider = GetComponent<BoxCollider2D>();
+    }
+
+    internal void Move(float direction, float speed)
+    {
+        Vector2 newVel = rb.velocity;
+
+        // Set x as movement
+        newVel.x = direction * speed;
+
+        // Set new velocity
+        rb.velocity = newVel;
+    }
+
+    internal void Jump()
+    {
+        if (CanJump())
+            rb.AddForce(Vector3.up * jumpForce);
+    }
+
+    private bool CanJump()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector3.up, distFromGround, LayerMask.NameToLayer(GroundLayer));
+
+        Debug.DrawLine(transform.position, -Vector3.up * distFromGround, Color.red);
+
+        return (hit.collider != null);
+    }
+}
---
+++ Gunsplosion Builder-Oh/Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Moving_Entity
+{
+    private void FixedUpdate()
+    {
+        PlayerInput();
+    }
+
+    void PlayerInput()
+    {
+        Move(Input.GetAxis("Horizontal"), moveSpeed);
+
+        if (Input.GetButtonDown("Jump"))
+            Jump();
+    }
+}
