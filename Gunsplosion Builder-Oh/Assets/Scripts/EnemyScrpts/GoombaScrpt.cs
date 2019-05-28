3f6535f13cc8879dc5e9262a2f3e0a4a37835ed5
Edward Goodhew <ejgoodhew@btinternet.com>
2019-05-28 13:56:22 +0100

    Nodes added to enemies

---
+++ Gunsplosion Builder-Oh/Assets/Scripts/EnemyScrpts/GoombaScrpt.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoombaScrpt : MonoBehaviour
+{
+    private Vector2 leftNodePos, rightNodePos;
+    public GameObject leftNode, rightNode;
+    private CircleCollider2D detectionRing;
+    // Start is called before the first frame update
+    void Start()
+    {
+        leftNodePos = leftNode.transform.position;
+        rightNodePos = rightNode.transform.position;
+        detectionRing = gameObject.GetComponent<CircleCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
