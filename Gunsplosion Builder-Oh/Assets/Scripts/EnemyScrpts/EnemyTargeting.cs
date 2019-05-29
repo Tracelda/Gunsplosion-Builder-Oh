b1c6ec52e2438db1d4bd2077bc074457cefa5987
Edward Goodhew <ejgoodhew@btinternet.com>
2019-05-29 16:40:48 +0100

    Work on enemy shooting

---
+++ Gunsplosion Builder-Oh/Assets/Scripts/EnemyScrpts/EnemyTargeting.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTargeting : MonoBehaviour
+{
+    public bool targetingPlayer;
+    public float distance, angle;
+    public GameObject playerObject;
+    public Vector3 shootDirection, normShootDirection;
+
+    private Moving_Entity Moving_Entity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Moving_Entity = gameObject.GetComponent<Moving_Entity>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (targetingPlayer)
+        {
+            FindAngle(gameObject.transform.position, playerObject.transform.position);
+            ShootAtPlayer();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            targetingPlayer = true;
+            playerObject = collision.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            targetingPlayer = false;
+            playerObject = null;
+        }
+    }
+
+    public void FindAngle(Vector3 enemyLocation, Vector3 playerPos)
+    {
+        shootDirection = (playerPos - enemyLocation);
+        normShootDirection = Vector3.Normalize(shootDirection);
+        
+        RaycastHit2D hit = Physics2D.Raycast(enemyLocation, normShootDirection);
+        Debug.DrawRay(enemyLocation, normShootDirection, Color.green);
+    }
+
+    public void ShootAtPlayer()
+    {
+        //Moving_Entity.Aim(normShootDirection.x, normShootDirection.y);
+        Moving_Entity.Aim(shootDirection.x, shootDirection.y);
+        Debug.Log("Shooting at player");
+    }
+
+    
+}
