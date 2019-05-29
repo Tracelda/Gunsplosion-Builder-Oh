5863dfcff35e17b1477603c7dcbca9a180116607
Widdiful <Widdiful@users.noreply.github.com>
2019-05-29 18:25:30 +0100

    Gun arm, some particle effects

---
+++ Gunsplosion Builder-Oh/Assets/Scripts/Player/GunArm.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunArm : MonoBehaviour
+{
+    public Transform target;
+    public SpriteRenderer sprite;
+
+    void Update()
+    {
+        transform.right = target.position - transform.position;
+        print(transform.rotation.eulerAngles.z);
+        if (transform.localRotation.eulerAngles.z == 135 || transform.localRotation.eulerAngles.z == 225) {
+            sprite.flipY = true;
+        }
+        else {
+            sprite.flipY = false;
+        }
+    }
+}
