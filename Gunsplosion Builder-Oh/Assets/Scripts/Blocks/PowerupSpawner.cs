3216d632b0725a130ced0f5e062ea2160a6285ba
Widdiful <Widdiful@users.noreply.github.com>
2019-05-30 16:17:24 +0100

    Enemy sprites, powerups in editor

---
+++ Gunsplosion Builder-Oh/Assets/Scripts/Blocks/PowerupSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerupSpawner : BlockResetBase
+{
+    private GameObject spawnedPickup;
+    public int pickupID;
+    public GameObject prefab;
+    public SpriteRenderer sprite;
+    public Abilities.abilityType type;
+
+    public void StartGame()
+    {
+        spawnedPickup = Instantiate(prefab, transform.position, Quaternion.identity);
+        spawnedPickup.GetComponent<AbilityPickup>().ability = type;
+        sprite.enabled = false;
+    }
+
+    public void ResetToEdit()
+    {
+        Destroy(spawnedPickup);
+        sprite.enabled = true;
+    }
+}
--- Gunsplosion Builder-Oh/Assets/Scripts/Effects/EffectManager.cs
+++ Gunsplosion Builder-Oh/Assets/Scripts/Effects/EffectManager.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class EffectManager : MonoBehaviour
 {
-    public enum ParticleTypes { StraightShot, BounceShot, HomingShot, SmallExplosion, LargeExplosion, HugeExplosion, SmallDust, LargeDust, ArmourDestroy};
-    public GameObject straightShot, bounceShot, homingShot, smallExplosion, largeExplosion, hugeExplosion, smallDust, largeDust, armourDestroy;
+    public enum ParticleTypes { StraightShot, BounceShot, HomingShot, SmallExplosion, LargeExplosion, HugeExplosion, SmallDust, LargeDust, ArmourDestroy, PlayerDestroy};
+    public GameObject straightShot, bounceShot, homingShot, smallExplosion, largeExplosion, hugeExplosion, smallDust, largeDust, armourDestroy, playerDestroy;
 
     public static EffectManager instance;
 
@@ -49,6 +49,8 @@ public class EffectManager : MonoBehaviour
                 return largeDust;
             case ParticleTypes.ArmourDestroy:
                 return armourDestroy;
+            case ParticleTypes.PlayerDestroy:
+                return playerDestroy;
         }
         return null;
     }
